fe989e446694addd8e937900d2576f23756497db
Andrey Arzamaskov <arzamaskov@gmail.com>
2024-11-29 17:17:59 +0400

    Create User entity

---
+++ backend/src/Shared/Domain/Service/UlidService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Shared\Domain\Service;
+
+class UlidService
+{
+    public static function generate(): string
+    {
+        return UlidService::generate();
+    }
+}
--- backend/src/Users/Domain/Entity/User.php
+++ backend/src/Users/Domain/Entity/User.php
@@ -2,7 +2,33 @@
 
 namespace App\Users\Domain\Entity;
 
+use App\Shared\Domain\Service\UlidService;
+
 class User
 {
-    // Class content
+    private string $ulid;
+    private string $email;
+    private string $password;
+
+    public function __construct(string $email, string $password)
+    {
+        $this->ulid = UlidService::generate();
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getUlid(): string
+    {
+        return $this->ulid;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
 }
