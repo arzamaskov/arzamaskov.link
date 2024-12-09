143923852876418e975d0c3e3bb3ad8e6cf86cae
Andrey Arzamaskov <arzamaskov@gmail.com>
2024-12-09 20:57:12 +0400

    Create User entity:
    - create User entity
    - create UserFactory
    - create UserRepository
    - create UserRepositoryTest
    - create migration
    - set up Doctrine ORM XML mapping

--- backend/src/Users/Domain/Entity/User.php
+++ backend/src/Users/Domain/Entity/User.php
@@ -2,7 +2,87 @@
 
 namespace App\Users\Domain\Entity;
 
+use App\Shared\Domain\Service\UlidService;
+
 class User
 {
-    // Class content
+    private string $ulid;
+    private string $email;
+    private string $password;
+    private array $roles = [];
+
+    public function __construct(string $email, string $password)
+    {
+        $this->ulid = UlidService::generate();
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getUlid(): string
+    {
+        return $this->ulid;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param array<int,mixed> $roles
+     */
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see Symfony\Component\Security\Core\User\UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+    }
 }
--- backend/src/Users/Infrastructure/Repository/UserRepository.php
+++ backend/src/Users/Infrastructure/Repository/UserRepository.php
@@ -2,9 +2,33 @@
 
 namespace App\Users\Infrastructure\Repository;
 
+use App\Users\Domain\Entity\User;
 use App\Users\Domain\Repository\UserRepositoryInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
-class UserRepository implements UserRepositoryInterface
+/**
+ * @extends ServiceEntityRepository<object>
+ */
+class UserRepository extends ServiceEntityRepository implements UserRepositoryInterface
 {
-    // Class content
+    private EntityManagerInterface $em;
+
+    public function __construct(ManagerRegistry $registry, EntityManagerInterface $em)
+    {
+        parent::__construct($registry, User::class);
+        $this->em = $em;
+    }
+
+    public function add(User $user): void
+    {
+        $this->em->persist($user);
+        $this->em->flush();
+    }
+
+    public function findByUlid(string $ulid): User
+    {
+        return $this->find($ulid);
+    }
 }
